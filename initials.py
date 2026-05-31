31f9222726002432c10a9bcf7de19f9c5fb278de
Angela Mensah Boateng <2425400973@live.gctu.edu.gh>
2026-05-31 19:34:06 +0000

    Completed Assignment

--- initials.py
+++ initials.py
@@ -3,5 +3,34 @@ def main():
   pass #code goes here
 
 
+# File: initials.py
+# Description: Print out my initials.
+# Assignment Number: 1
+#
+# Name: Angela Mensah Boateng
+# STUDENT ID:  2425400973
+# Email: 2425400973@live.gctu.edu.gh
+# Grader: Augustus
+#
+# On my honor, Angela Mensah Boateng, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
+
+
+  # Print initials A, M, B in stylized large block letters using their own letters and periods
+
+  print()
+  print("...AMB")
+  print()
+  print(".......AAA.............MMMM....MMMM........BBBBBBBBBBB....")
+  print(".....AA...AA...........MMMM....MMMM........BBB......BBB...")
+  print("....AA......AA.........MM.MM..MM.MM........BBB......BBB...")
+  print("...AAA......AAA........MM.MM..MM.MM........BBB......BBB...")
+  print("...AAAAAAAAAAAA........MM.MM..MM.MM........BBBBBBBBBBBB...")
+  print("...AA........AA........MM..MMMM..MM........BBB......BBB...")
+  print("...AA........AA........MM..MMMM..MM........BBB......BBB...")
+  print("...AA........AA........MM...MM...MM........BBB......BBB...")
+  print("...AA........AA...**...MM...MM...MM...**...BBBBBBBBBBBB...")
+  print("...AA........AA...**...MM...MM...MM...**...BBBBBBBBBBB....")
 
 main()
